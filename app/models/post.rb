aaeca80c213f2b7c6cc4583f875b34a13b9b5a58
github username <github email address>
2020-03-17 11:19:15 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-fiction) }
+  validate :Is_Clicky
+
+  Clickable = [
+        /Won't/,
+        /Secret/,
+        /Top/,
+        /Guess/
+      ]
+
+  def Is_Clicky
+    if Clickable.none? { |pattern| pattern.match title}
+      errors.add(:title, "Your title needs to be clickbait!")
+    end
+  end
 end
